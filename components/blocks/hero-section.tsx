d88c328cffffbf510c2b96b4b799e4178a4c2937
matiasvlevi <matiasvlevi@gmail.com>
2026-02-22 22:37:21 -0500

    better slogan line breaks

--- components/blocks/hero-section.tsx
+++ components/blocks/hero-section.tsx
@@ -1,27 +1,37 @@
 import { cn } from "@/lib/utils";
 import LocaleToggle from "../LocaleToggle";
 import { getLocale } from "next-intl/server";
+import { Fragment } from "react";
 
 interface HeroSectionProps {
   title?: string;
   subtitle?: string;
-  locale: 'en'|'fr';
+  locale: 'en' | 'fr';
 }
 
-export async function HeroSection({ title, subtitle, locale }: HeroSectionProps) { 
+export async function HeroSection({ title, subtitle, locale }: HeroSectionProps) {
+
+  const t = (subtitle || '').split('\\n');
 
   return (
     <section className="relative min-h-[30vh] sm:min-h-[38vh] md:min-h-[42vh] xl:min-h-[54vh] flex items-start justify-start bg-primary text-primary-foreground px-10 py-16 sm:py-10 pb-9">
 
-      <LocaleToggle locale={locale}/>
+      <LocaleToggle locale={locale} />
       {/* Hero content */}
-      <div className={cn("mt-8 sm:mt-16", "max-w-2/3 xl:max-w-2xl")}>
-        <h1 className="font-sans uppercase italic text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold leading-[1.05] mb-4 text-balance">
-          {title}
-        </h1>
+      <div className={cn("mt-8 sm:mt-16")}>
+        <div className="max-w-1/3 xl:max-w-2xl">
+          <h1 className="font-sans uppercase italic text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-bold leading-[1.05] mb-4 text-balance">
+            {title}
+          </h1>
+        </div>
         {subtitle && (
-          <p className="font-sans text-lg sm:text-2xl md:text-3xl lg:text-4xl font-medium leading-tight">{subtitle}</p>
+          <p className="font-sans text-lg sm:text-2xl md:text-3xl lg:text-4xl font-medium leading-tight">{
+            t.map((line, i) => {
+              return <Fragment key={i}>{line}{((t.length > 1) && i !== t.length - 1) ? <br /> : <></>}</Fragment>
+            })
+          }</p>
         )}
+
       </div>
     </section>
   )
