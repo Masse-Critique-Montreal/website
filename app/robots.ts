2193c51053742a8ea075b9e69e374d6c780e2513
matiasvlevi <matiasvlevi@gmail.com>
2026-03-03 18:10:21 -0500

    robots txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 // app/robots.ts  (App Router)
 import { MetadataRoute } from 'next'
-
+export const dynamic = 'force-static' 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
