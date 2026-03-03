4ca73f03333d664dd8e11371758baca98bcbf1c3
matiasvlevi <matiasvlevi@gmail.com>
2026-03-03 18:09:22 -0500

    robots txt

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+// app/robots.ts  (App Router)
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+  }
+}
